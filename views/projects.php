f48311e94872e38b8be9e159d70296ab5edc7fa6
Enzo <fournierenzo62@gmail.com>
2025-07-07 01:07:56 +0200

    Update projects.php

--- views/projects.php
+++ views/projects.php
@@ -8,6 +8,8 @@
     <title>Enzo Fournier - Développeur</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
+
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <!-- Google Fonts -->
@@ -318,6 +320,17 @@
         }
     </style>
 </head>
+
+<div class="container-fluid">
+        <!-- Header Section -->
+        <div class="profile-header fade-in">
+            <div class="profile-avatar">
+                <i class="bi bi-laptop"></i>
+            </div>
+            <h1>Mes projets</h1>
+            <p class="lead text-secondary">Voici la plupart des projets que j'ai pu réalisé</p>
+        </div>
+</div>
 
 <!-- Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>

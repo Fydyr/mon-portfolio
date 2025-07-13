4a43e520b7dd3bec74cefe35c84d27f08f67b675
Enzo <fournierenzo62@gmail.com>
2025-07-13 18:06:14 +0200

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,13 +7,8 @@ class AdminController extends BaseController
     public function admin()
     {
         if (!isset($_SESSION['user_id']) || !isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
-            global $router;
-            if (method_exists($router, 'call')) {
-                $router->call('403');
-            } else {
                 header('HTTP/1.1 403 Forbidden');
                 echo view('403', ['title' => '403 - Accès interdit']);
-            }
             exit;
         }
         echo $this->view('admin');
